1594ee7839e86e54979edca523c64dabe4162f3b
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-22 14:13:40 -0700

    debug

--- cs336_systems/flash_attention.py
+++ cs336_systems/flash_attention.py
@@ -213,11 +213,11 @@ def flash_fwd_kernel(
 
         rowmax = tl.max(S_ij, axis=-1)
 
-        # m_ij = tl.maximum(m[:], 
-        #                      rowmax) 
+        m_ij = tl.maximum(m[:], 
+                             rowmax) 
 
-        # P_ij = tl.exp(S_ij - tl.view(m_ij, (Q_TILE_SIZE, 1))) 
-        # # assert P_ij.shape == (Q_TILE_SIZE, K_TILE_SIZE)
+        P_ij = tl.exp(S_ij - tl.view(m_ij, (Q_TILE_SIZE, 1))) 
+        # assert P_ij.shape == (Q_TILE_SIZE, K_TILE_SIZE)
 
         # l = tl.exp(m[:] - m_ij) * l[:] + tl.sum(P_ij, axis=-1)
         # # reduce(P_ij, "B_q B_k -> B_q", 'sum') 

e240fcff5569c652c3562a0ca0f094ed9c95dd3e
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-22 12:28:28 -0700

    minor

--- cs336_systems/experiments.py
+++ cs336_systems/experiments.py
@@ -135,6 +135,7 @@ def part_1_1_6():
 
     for context_len in context_lens:
         for model in models:
+            print(f'running model {model}_{context_len}')
             memory_profiling(f'{model}_{context_len}',
                              4, 10000, context_len, 
                              model_list[model]['d_model'], 

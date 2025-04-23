57738ab6b3e021d7f4faaae931e461d0307408ad
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-22 17:09:06 -0700

    debug

--- cs336_systems/flash_attention.py
+++ cs336_systems/flash_attention.py
@@ -225,12 +225,12 @@ def flash_fwd_kernel(
         # l2 = tl.sum(P_ij, axis=-1)
         
         # O = tl.dot(diag, O)
-        diag = tl.exp(m[:] - m_ij[:])
-        O2 = O * diag[:, None]
-        O2 += tl.dot(P_ij, V_j)
+        # diag = tl.exp(m[:] - m_ij[:])
+        # O2 = O * diag[:, None]
+        # O2 += tl.dot(P_ij, V_j)
 
-        K_tile_ptr = K_tile_ptr.advance((K_TILE_SIZE,))
-        V_tile_ptr = V_tile_ptr.advance((K_TILE_SIZE,))
+        # K_tile_ptr = K_tile_ptr.advance((K_TILE_SIZE,))
+        # V_tile_ptr = V_tile_ptr.advance((K_TILE_SIZE,))
 
     tl.store(O_tile_ptr, 
              O2 / l2[:, None],

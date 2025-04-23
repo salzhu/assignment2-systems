e1423d04ba665f96170aedb34e8d954b5e4f0204
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-22 17:43:08 -0700

    debug

--- cs336_systems/flash_attention.py
+++ cs336_systems/flash_attention.py
@@ -230,6 +230,8 @@ def flash_fwd_kernel(
         K_tile_ptr = K_tile_ptr.advance((0,K_TILE_SIZE))
         V_tile_ptr = V_tile_ptr.advance((0,K_TILE_SIZE))
 
+        m = m_ij 
+
     # tl.device_print("m", m)
     tl.device_print("S_ij", S_ij)
     tl.device_print("rowmax", rowmax)

f9886e57fc26f4467060e433f85a966e3e7956b1
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-22 17:15:59 -0700

    debug

--- cs336_systems/flash_attention.py
+++ cs336_systems/flash_attention.py
@@ -220,7 +220,7 @@ def flash_fwd_kernel(
         P_ij = tl.exp(S_ij - tl.view(m_ij, (Q_TILE_SIZE, 1))) 
         # assert P_ij.shape == (Q_TILE_SIZE, K_TILE_SIZE)
 
-        tl.device_print("shapes", m.shape, m_ij.shape)
+        tl.device_print("shapes", m.shape)
 
         # l2 = tl.exp(m - m_ij) * l #+ tl.sum(P_ij, axis=-1) # bad line
         l2 = tl.exp(m - m_ij) * l

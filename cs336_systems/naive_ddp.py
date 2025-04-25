f31c5ca0f73150ab75be39ec51e93aa93d1c4c7c
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-25 09:08:37 -0700

    naive ddp flat

--- cs336_systems/naive_ddp.py
+++ cs336_systems/naive_ddp.py
@@ -95,19 +95,112 @@ def data_parallelism_main(rank, world_size, data_in, data_targ, weights,
     # state_dicts.append(model.state_dict())
     cleanup()
 
+def ddp_flat_main(rank, world_size, data_in, data_targ, weights, 
+                          vocab_size, context_length, d_model, num_layers, num_heads, d_ff, rope_theta, batch_size, 
+                          state_dicts, step_times, grad_collect_times):
+    # torch.cuda.set_device(rank)
+    setup(rank, world_size)
+
+    # Get the slice of data for this rank (in practice, each rank should load only its own data)
+    # batch_size = data.size(0)  # @inspect batch_size
+    # num_dim = data.size(1)  # @inspect num_dim
+    local_batch_size = batch_size // world_size  # @inspect local_batch_size
+    start_index = rank * local_batch_size  # @inspect start_index
+    end_index = start_index + local_batch_size  # @inspect end_index
+    data_in = data_in[:,start_index:end_index].cuda(rank) # n_steps x 2 x data
+    data_targ = data_targ[:,start_index:end_index].cuda(rank) # n_steps x 2 x data
+
+    # use params passed in to load a transformer 
+    model = BasicsTransformerLM(vocab_size, context_length, d_model, num_layers, num_heads, d_ff, rope_theta)
+    model.load_state_dict(weights)
+    model = model.cuda(rank)
+
+    # train one step 
+    # loss.backward() and get gradients, reduce 
+    # optimizer.step()
+
+    # vocab of 10000, batch size of 4 
+    
+    # Create MLP parameters params[0], ..., params[num_layers - 1] (each rank has all parameters)
+    # params = [get_init_params(num_dim, num_dim, rank) for i in range(num_layers)]
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)  # Each rank has own optimizer state
+
+    num_steps = data_in.shape[0]
+    warmup_steps = 0
+
+    for step in range(num_steps):
+        torch.cuda.synchronize()
+
+        start_time_step = timeit.default_timer()
+
+        print(f"Rank {rank} train step {step}")
+        # Forward pass
+        inputs = data_in[step]
+        targets = data_targ[step]
+
+        outputs = model(inputs)
+
+        outputs = outputs.view(-1, outputs.size(-1))
+        targets = targets.view(-1)
+        
+        loss = cross_entropy(outputs, targets)
+        loss.backward()
+
+        torch.cuda.synchronize()
+
+        start_time_grad = timeit.default_timer()
+        
+        # Sync gradients across workers (only difference between standard training and DDP)
+        # flatten first 
+        # get flat gradient: param_flat
+        # dist.all_reduce(tensor=param_flat, op=dist.ReduceOp.AVG, async_op=False)
+        # put back into gradients 
+
+        param_list = model.state_dict()
+        
+        params_flat = torch._utils._flatten_dense_tensors([tensor for tensor in param_list.values()])
+        dist.all_reduce(tensor=params_flat, op=dist.ReduceOp.AVG, async_op=False)
+
+        unflattened = torch._utils._unflatten_dense_tensors(params_flat, [tensor for tensor in param_list.values()])
+        new_state_dict = {}
+        for (key, _), tensor in zip(param_list.items(), unflattened):
+            new_state_dict[key] = tensor
+
+        # for param in model.parameters():
+        #     dist.all_reduce(tensor=param.grad, op=dist.ReduceOp.AVG, async_op=False)
+        model.load_state_dict(new_state_dict)
+
+        torch.cuda.synchronize()
+
+        end_time_grad = timeit.default_timer()
+        
+        # Update parameters
+        optimizer.step()
+        end_time_step = timeit.default_timer()
+        
+        params = model.state_dict()
+        print(f"[data_parallelism] Rank {rank}: step = {step}, loss = {loss.item()}, params = {params['layers.1.ln1.weight']}", flush=True)
+
+        if step >= warmup_steps: 
+            step_times.append(end_time_step - start_time_step)
+            grad_collect_times.append(end_time_grad - start_time_grad)
+    
+    # state_dicts.append(model.state_dict())
+    cleanup()
+
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     world_size = 2
 
-    n = 20
+    n = 4
     batch_size = 4
     context_length = 256
 
-    vocab_size = 10000
-    d_model = 1600
-    num_layers = 48
-    d_ff = 6400
-    num_heads = 25
+    vocab_size = 100 # 10000
+    d_model = 16 # 1600
+    num_layers = 2 # 48
+    d_ff = 64 # 6400
+    num_heads = 2 # 25
     rope_theta = 10000
 
     model = BasicsTransformerLM(vocab_size, context_length, d_model, num_layers, num_heads, d_ff, rope_theta)

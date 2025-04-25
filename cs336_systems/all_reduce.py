d1372374b21e7e21698254e928a67bc8d47efde3
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-24 23:05:20 -0700

    all reduce gpu

--- cs336_systems/all_reduce.py
+++ cs336_systems/all_reduce.py
@@ -14,7 +14,7 @@ lock = threading.Lock()
 
 def setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "28500"
+    os.environ["MASTER_PORT"] = "29500"
     dist.init_process_group("nccl", rank=rank, world_size=world_size) # gloo, nccl
 
 def all_reduce_time(rank, data, world_size):
@@ -29,6 +29,7 @@ def all_reduce(rank, world_size, tensor, result):
     # Create tensor
     # tensor = torch.randn(num_elements, device=f'cuda:{rank}')
     # Warmup
+    tensor.to(f'cuda:{rank}')
     dist.all_reduce(tensor=tensor, op=dist.ReduceOp.SUM, async_op=False)
     if torch.cuda.is_available():
         torch.cuda.synchronize()  # Wait for CUDA kernels to finish

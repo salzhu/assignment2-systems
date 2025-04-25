5f01f271f3e84a80c02cf93bf746fb7c0e682433
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-24 23:28:58 -0700

    all reduce gpu

--- cs336_systems/all_reduce.py
+++ cs336_systems/all_reduce.py
@@ -15,7 +15,7 @@ lock = threading.Lock()
 def setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = "29500"
-    dist.init_process_group("gloo", rank=rank, world_size=world_size) # gloo, nccl
+    dist.init_process_group("nccl", rank=rank, world_size=world_size) # gloo, nccl
 
 def all_reduce_time(rank, data, world_size):
     

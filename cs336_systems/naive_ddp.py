0b5e1252fd388f8c5f23c52562a0f2461ab5700d
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-25 00:32:41 -0700

    naive ddp testing

---
+++ cs336_systems/naive_ddp.py
@@ -0,0 +1,95 @@
+import os
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+
+import timeit 
+import pandas as pd 
+
+from cs336_basics.model import BasicsTransformerLM 
+from cs336_basics.nn_utils import cross_entropy
+
+def setup(rank, world_size):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "29500"
+    dist.init_process_group("nccl", rank=rank, world_size=world_size) # gloo
+
+def cleanup():
+    dist.destroy_process_group()
+
+def data_parallelism_main(rank, world_size, data_in, data_targ, weights,  
+                          vocab_size, context_length, d_model, num_layers, num_heads, d_ff, rope_theta, batch_size=4):
+    # torch.cuda.set_device(rank)
+    setup(rank, world_size)
+
+    # Get the slice of data for this rank (in practice, each rank should load only its own data)
+    # batch_size = data.size(0)  # @inspect batch_size
+    # num_dim = data.size(1)  # @inspect num_dim
+    local_batch_size = batch_size // world_size  # @inspect local_batch_size
+    start_index = rank * local_batch_size  # @inspect start_index
+    end_index = start_index + local_batch_size  # @inspect end_index
+    data_in = data_in[:,start_index:end_index]#.cuda(rank) # n_steps x 2 x data
+    data_targ = data_targ[:,start_index:end_index]#.cuda(rank) # n_steps x 2 x data
+
+    # use params passed in to load a transformer 
+    model = BasicsTransformerLM(vocab_size, context_length, d_model, num_layers, num_heads, d_ff, rope_theta)
+    model.load_state_dict(weights)
+
+    # train one step 
+    # loss.backward() and get gradients, reduce 
+    # optimizer.step()
+
+    # vocab of 10000, batch size of 4 
+    
+    # Create MLP parameters params[0], ..., params[num_layers - 1] (each rank has all parameters)
+    # params = [get_init_params(num_dim, num_dim, rank) for i in range(num_layers)]
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)  # Each rank has own optimizer state
+
+    num_steps = data_in.shape[0]
+
+    for step in range(num_steps):
+        # Forward pass
+        inputs = data_in[step]
+        targets = data_targ[step]
+
+        outputs = model(inputs)
+
+        outputs = outputs.view(-1, outputs.size(-1))
+        targets = targets.view(-1)
+        
+        loss = cross_entropy(outputs, targets)
+        loss.backward()
+        
+        # Sync gradients across workers (only difference between standard training and DDP)
+        for param in model.parameters():
+            dist.all_reduce(tensor=param.grad, op=dist.ReduceOp.AVG, async_op=False)
+        
+        # Update parameters
+        optimizer.step()
+        print(f"[data_parallelism] Rank {rank}: step = {step}, loss = {loss.item()}, params = {model.parameters()}", flush=True)
+    
+    cleanup()
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    world_size = 2
+
+    n = 4
+    batch_size = 4
+    context_length = 256
+
+    vocab_size = 10000
+    d_model = 1600
+    num_layers = 48
+    d_ff = 6400
+    num_heads = 25
+    rope_theta = 10000
+
+    model = BasicsTransformerLM(vocab_size, context_length, d_model, num_layers, num_heads, d_ff, rope_theta)
+
+    data_in = torch.randint(0,vocab_size,(n, batch_size, context_length), device=device)
+    data_targ = torch.randint(0,vocab_size,(n, batch_size, context_length), device=device)
+
+    mp.spawn(data_parallelism_main, args=(world_size,data_in, data_targ, model.parameters(),
+                                          vocab_size, context_length, d_model, num_layers, num_heads, d_ff, rope_theta), 
+             nprocs=world_size, join=True)

9abdee5e769ff08a561b662bb6d704e341bca4f5
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-25 18:19:41 -0700

    flash benchmarking

--- cs336_systems/benchmark_flash.py
+++ cs336_systems/benchmark_flash.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
         128
     ]
     context_lens = [128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]
-    context_lens = [128, 256, 1024, 4096, 16384, 65536]
+    # context_lens = [128, 256, 1024, 4096, 16384, 65536]
     dtypes = [torch.float32, torch.bfloat16]
 
     for dtype in dtypes: 

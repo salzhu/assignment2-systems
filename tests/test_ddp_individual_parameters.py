1bf32e4f2c9b27a2a1ed1fbcad197fe7c8739ca1
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-26 19:10:18 -0700

    testing

--- tests/test_ddp_individual_parameters.py
+++ tests/test_ddp_individual_parameters.py
@@ -50,7 +50,7 @@ def _test_DistributedDataParallelIndividualParameters(rank: int, world_size: int
     # Create a toy model and move it to the proper device.
     # This is our non-parallel baseline.
     print('***********************************')
-    print(device)
+    print(device, rank)
     print('***********************************')
     non_parallel_model = model_class().to(device)
 

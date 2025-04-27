576bb1a07fd9a2d525eec990290927e0a10cd6de
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-26 19:34:51 -0700

    testing

--- tests/common.py
+++ tests/common.py
@@ -72,19 +72,19 @@ def _setup_process_group(rank, world_size, backend):
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = "29500" # "12390"
     # https://discuss.pytorch.org/t/should-local-rank-be-equal-to-torch-cuda-current-device/150873/2
-    # if torch.cuda.is_available():
-    #     device_count = torch.cuda.device_count()
-    #     print(f'device count {device_count}')
-    #     local_rank = None
-    #     if device_count > 0:
-    #         local_rank = rank % device_count
-    #         torch.cuda.set_device(local_rank)
-    #     else:
-    #         raise ValueError("Unable to find CUDA devices.")
-    #     device = f"cuda:{local_rank}"
-    # else:
-    #     device = "cpu"
-    device = f"cuda:{rank}"
+    if torch.cuda.is_available():
+        device_count = torch.cuda.device_count()
+        print(f'device count {device_count}')
+        local_rank = None
+        if device_count > 0:
+            local_rank = rank % device_count
+            torch.cuda.set_device(local_rank)
+        else:
+            raise ValueError("Unable to find CUDA devices.")
+        device = f"cuda:{local_rank}"
+    else:
+        device = "cpu"
+    # device = f"cuda:{rank}"
     # initialize the process group
     dist.init_process_group(backend, rank=rank, world_size=world_size)
     print('@@@@@@@@@@@@@@@@@@@@')

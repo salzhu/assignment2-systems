69689ab4376c9d3643f6ab96c8e41bd0f2931d13
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-26 19:20:06 -0700

    testing

--- tests/common.py
+++ tests/common.py
@@ -70,7 +70,7 @@ class ToyModelWithTiedWeights(nn.Module):
 
 def _setup_process_group(rank, world_size, backend):
     os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "12390"
+    os.environ["MASTER_PORT"] = "29500" # "12390"
     # https://discuss.pytorch.org/t/should-local-rank-be-equal-to-torch-cuda-current-device/150873/2
     if torch.cuda.is_available():
         device_count = torch.cuda.device_count()

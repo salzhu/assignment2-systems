65d8be96f47010fcf425a8899a57fd2b9809e479
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-28 08:55:42 -0700

    flash bfloat16

--- cs336_systems/benchmark_flash.py
+++ cs336_systems/benchmark_flash.py
@@ -8,6 +8,50 @@ import argparse
 
 from flash_attention import FlashAttentionTriton
 
+from cs336_basics.model import scaled_dot_product_attention
+
+class Attention(nn.Module):
+    def forward(self, Q, K, V):
+        return scaled_dot_product_attention(Q, K, V)
+
+def pytorch_attn(batch_size, dim, seq_len, n=100, w=10):
+    # make the attention module 
+    # make random inputs Q, K, V of size batch_size x seq_len x dim 
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    rand_Q = torch.randn(batch_size, seq_len, dim, device=device, requires_grad=True)
+    rand_K = torch.randn(batch_size, seq_len, dim, device=device, requires_grad=True)
+    rand_V = torch.randn(batch_size, seq_len, dim, device=device, requires_grad=True)
+
+    forward_time = []
+    backward_time = []
+    full_time = []
+
+    attn = Attention()
+
+    for i in range(w + n):
+        torch.cuda.synchronize()
+
+        start_time = timeit.default_timer()
+        out = attn(rand_Q, rand_K, rand_V)
+        # print(out)
+        # print(torch.sum(out, [0,1,2]))
+        torch.cuda.synchronize()
+        mid_time = timeit.default_timer()
+        
+        
+        torch.sum(out, [0,1,2]).backward()
+        torch.cuda.synchronize()
+        end_time = timeit.default_timer()
+
+        if i >= w:
+            forward_time.append(mid_time - start_time)
+            backward_time.append(end_time - mid_time)
+            full_time.append(end_time - start_time)
+
+
+    return np.mean(forward_time), np.mean(backward_time), np.mean(full_time)
+
 def flash_attn_triton(dim, seq_len, dtype, n=100, w=10):
     # make the attention module 
     # make random inputs Q, K, V of size batch_size x seq_len x dim 
@@ -64,7 +108,7 @@ if __name__ == '__main__':
     context_lens = [128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]
     # context_lens = [128, 256, 1024, 4096, 16384, 65536]
     # dtypes = [torch.float32, torch.bfloat16]
-    dtypes = [torch.bfloat16]
+    dtypes = [torch.float32]
 
     for dtype in dtypes: 
         for dim in dims:
@@ -77,17 +121,20 @@ if __name__ == '__main__':
                 #     ft, bt = pytorch_compiled_attn(8, dim, context_len)
 
                 # use tile size of 16 for all
-                with torch.autocast(device_type='cuda',dtype=dtype):
+                # with torch.autocast(device_type='cuda',dtype=dtype):
+                if args.triton == 1:
                     forward, backward, full = flash_attn_triton(dim, context_len, dtype)
+                elif args.triton == 0:
+                    forward, backward, full = pytorch_attn(dim, context_len, dtype)
 
-                    df['model dim'].append(dim)
-                    df['seq len'].append(context_len)
-                    df['dtype'].append(str(dtype))
-                    df['forward time (ms)'].append(1000 * forward)
-                    df['backward time (ms)'].append(1000 * backward)
-                    df['full step time (ms)'].append(1000 * full)
+                df['model dim'].append(dim)
+                df['seq len'].append(context_len)
+                df['dtype'].append(str(dtype))
+                df['forward time (ms)'].append(1000 * forward)
+                df['backward time (ms)'].append(1000 * backward)
+                df['full step time (ms)'].append(1000 * full)
 
-                    print(1000 * forward, 1000 * backward)
+                print(1000 * forward, 1000 * backward)
                     # except: 
                     #     df['model dim'].append(dim)
                     #     df['seq len'].append(context_len)

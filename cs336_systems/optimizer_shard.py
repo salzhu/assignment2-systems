80d769a87bbb387955a4988b4b90edde5e4b20a8
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-27 21:22:06 -0700

    sharded opt

--- cs336_systems/optimizer_shard.py
+++ cs336_systems/optimizer_shard.py
@@ -119,5 +119,5 @@ class OptimizerSharded(torch.optim.Optimizer):
         # raise NotImplementedError
     
     def add_param_group(self, param_group: dict[str, Any]):
-        self.optim.add_param_group(param_group)
+        self.opt.add_param_group(param_group)
         # raise NotImplementedError

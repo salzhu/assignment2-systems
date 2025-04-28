9b8bdc0719587154e9f7c4c71ef58dcea9af981a
salzhu <163059861+salzhu@users.noreply.github.com>
2025-04-27 17:51:58 -0700

    ddp overlap

--- tests/common.py
+++ tests/common.py
@@ -43,8 +43,8 @@ class ToyModel(nn.Module):
 
     def forward(self, x):
         x = self.relu(self.fc1(x))
-        # x = self.relu(self.fc2(x))
-        # x = self.fc3(x)
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
 
